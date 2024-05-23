96af9e4a59638e6869eb8cb5c4cb7b8f061a94a1
Bevergames <98055314+Bevergames2018@users.noreply.github.com>
2024-05-23 13:39:30 +0000

    2nd test

--- chip.py
+++ chip.py
@@ -1,5 +1,5 @@
-from gpt4all import GPT4ALL
-model = GPT4ALL("mistral-7b-instruct-v0.1.Q4_0.gguf")
+from gpt4all import GPT4All
+model = GPT4All("mistral-7b-instruct-v0.1.Q4_0.gguf")
 
 output = model.prompt("hello")
 print(output)
